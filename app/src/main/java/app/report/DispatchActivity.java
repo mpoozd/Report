47291cf1e4f0bef8ffb432a89a64e3388e0dd295
mpoozd <mpoozd@hotmail.com>
2015-08-08 22:49:08 +0300

    update reverting code

--- app/src/main/java/app/report/DispatchActivity.java
+++ app/src/main/java/app/report/DispatchActivity.java
@@ -1,10 +1,18 @@
 package app.report;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
 import com.parse.FindCallback;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
@@ -12,61 +20,48 @@ import com.parse.ParseFile;
 import com.parse.ParseImageView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-public class DispatchActivity extends ListActivity {
+public class DispatchActivity extends Activity {
 
-    ListView listview;
-    List<ParseObject> ob;
-    CustomAd adapter;
-    private List<Post> post = null;
 
+    private ParseQueryAdapter<ParseObject> mainAdapter;
+    private CustomAd urgentTodosAdapter;
+    private ListView listView;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispatch);
-    }
 
-    protected Void doInBackground(Void... params) {
-        // Create the array
-        post = new ArrayList<Post>();
-        try {
-            // Locate the class table named "Country" in Parse.com
-            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
-                    "Post");
-            // Locate the column named "ranknum" in Parse.com and order list
-            // by ascending
-            ob = query.find();
-            for (ParseObject Post : ob) {
-                // Locate images in flag column
-                ParseFile image = (ParseFile) Post.get("picture");
+        // Initialize main ParseQueryAdapter
+        mainAdapter = new ParseQueryAdapter<ParseObject>(this, "Post");
+        mainAdapter.setTextKey("build");
+        mainAdapter.setTextKey("park");
+        mainAdapter.setTextKey("plate");
+        mainAdapter.setImageKey("picture");
 
-                Post map = new Post();
-                map.setBuild((String) Post.get("build"));
-                map.setPark((String) Post.get("park"));
-                map.setPlate((String) Post.get("plate"));
-                map.setPicture(image.getUrl());
-                post.add(map);
+        // Initialize the subclass of ParseQueryAdapter
+        urgentTodosAdapter = new CustomAd(this);
+
+        // Initialize ListView and set initial view to mainAdapter
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(mainAdapter);
+        mainAdapter.loadObjects();
+
+// Initialize toggle button
+        if (listView.getAdapter() == mainAdapter) {
+            listView.setAdapter(urgentTodosAdapter);
+            urgentTodosAdapter.loadObjects();
             }
-        } catch (ParseException e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-        }
-        return null;
-    }
 
-    protected void onPostExecute(Void result) {
-        // Locate the listview in listview_main.xml
-        listview = (ListView) findViewById(android.R.id.list);
-        // Pass the results into ListViewAdapter.java
-        adapter = new CustomAd(DispatchActivity.this,
-                post);
-        // Binds the Adapter to the ListView
-        listview.setAdapter(adapter);
-        // Close the progressdialog
+        }
+
     }
-}
